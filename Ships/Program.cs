e56c67dfd36cd0f33b0a9e3e3ddfa181959ac08d
P4pryQ <149543457+P4pryQ@users.noreply.github.com>
2024-06-05 13:27:15 +0300

    Add files via upload

---
+++ Ships/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ships
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(0, 0);
+            game.startGame();
+        }
+    }
+}
---
+++ Ships/Ship.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ships
+{
+    internal class Ship
+    {
+        public int setFigure()
+        {
+            int parsedNumber;
+            while (true)
+            {
+                Console.WriteLine("Napisz cyfre pola:");
+                string figureShip = Console.ReadLine();
+                if (int.TryParse(figureShip, out parsedNumber) && parsedNumber > 0 && parsedNumber < 11)
+                {
+                    return parsedNumber - 1;
+                }
+            }
+        }
+
+        public int setLettler()
+        {
+            var letterToIndexMap = new Dictionary<string, int>
+            {
+                { "A", 0 },
+                { "B", 1 },
+                { "C", 2 },
+                { "D", 3 },
+                { "E", 4 },
+                { "F", 5 },
+                { "G", 6 },
+                { "H", 7 },
+                { "I", 8 },
+                { "J", 9 }
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Napisz literę pola:");
+                string lettlerShip = Console.ReadLine().ToUpper();
+
+                if (letterToIndexMap.TryGetValue(lettlerShip, out int index))
+                {
+                    return index;
+                }
+            }
+        }
+
+        public char setDirection()
+        {
+            Console.WriteLine("Podaj kierunek w który statek ma sie ustawić");
+            Console.WriteLine("V - Pionowo");
+            Console.WriteLine("H - Poziom");
+            string Direction = Console.ReadLine().ToUpper();
+            switch (Direction)
+            {
+                case "V":
+                    return 'V';
+                case "H":
+                default:
+                    return 'H';
+            }
+        }
+
+    }
+}
